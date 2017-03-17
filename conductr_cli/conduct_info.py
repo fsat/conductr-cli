7ca268a21dd82067cdbecead218d0c42f735c0f6
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-03-17 14:13:39 +1100

    Display single bundle - filter by name or id

--- conductr_cli/conduct_info.py
+++ conductr_cli/conduct_info.py
@@ -21,16 +21,28 @@ def info(args):
         log.verbose(validation.pretty_json(response.text))
 
     bundles = json.loads(response.text)
-    if args.quiet:
-        display_quiet(args, bundles)
+    if args.bundle:
+        display_bundle(args, bundles, args.bundle)
     else:
-        is_license_success, conductr_license = license.get_license(args)
-        display_default(args, is_license_success, conductr_license, bundles)
+        display_all(args, bundles)
 
     return True
 
 
-def display_default(args, is_license_success, conductr_license, bundles):
+def display_bundle(args, bundles, bundle_id_or_name):
+    bundles = filter_bundles_by_id_or_name(bundles, bundle_id_or_name)
+    print(bundles)
+
+
+def display_all(args, bundles):
+    if args.quiet:
+        display_all_quiet(args, bundles)
+    else:
+        is_license_success, conductr_license = license.get_license(args)
+        display_all_default(args, is_license_success, conductr_license, bundles)
+
+
+def display_all_default(args, is_license_success, conductr_license, bundles):
     log = logging.getLogger(__name__)
 
     if is_license_success:
@@ -79,7 +91,7 @@ def display_default(args, is_license_success, conductr_license, bundles):
         log.screen('There are errors: use `conduct events` or `conduct logs` for further information')
 
 
-def display_quiet(args, bundles):
+def display_all_quiet(args, bundles):
     log = logging.getLogger(__name__)
 
     for bundle in bundles:
@@ -90,3 +102,40 @@ def display_bundle_id(args, bundle):
     bundle_id = bundle['bundleId'] if args.long_ids else bundle_utils.short_id(bundle['bundleId'])
     has_error_display = '! ' if bundle.get('hasError', False) else ''
     return '{}{}'.format(has_error_display, bundle_id)
+
+
+def filter_bundles_by_id_or_name(bundles, bundle_id_or_name):
+    return [
+        bundle
+        for bundle in bundles
+        if has_bundle_name(bundle, bundle_id_or_name) or has_bundle_id(bundle, bundle_id_or_name)
+    ]
+
+
+def has_bundle_name(bundle, bundle_name):
+    actual_name = bundle['attributes']['bundleName']
+    return actual_name == bundle_name or actual_name.startswith(bundle_name)
+
+
+def has_bundle_id(bundle, bundle_id):
+    actual_bundle_id = bundle['bundleId']
+    if actual_bundle_id == bundle_id:
+        return True
+    else:
+        if '-' in actual_bundle_id and '-' in bundle_id:
+            actual_bundle_id_parts = actual_bundle_id.split('-')
+            actual_bundle_digest = actual_bundle_id_parts[0]
+            actual_config_digest = actual_bundle_id_parts[1]
+
+            bundle_id_parts = bundle_id.split('-')
+            bundle_digest = bundle_id_parts[0]
+            config_digest = bundle_id_parts[1]
+
+            bundle_digest_matches = actual_bundle_digest == bundle_digest or \
+                actual_bundle_digest.startswith(bundle_digest)
+            config_digest_matches = actual_config_digest == config_digest or \
+                actual_config_digest.startswith(config_digest)
+
+            return bundle_digest_matches and config_digest_matches
+        else:
+            return actual_bundle_id.startswith(bundle_id)
